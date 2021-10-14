70faab344c7ab9ff868a24ef5cfc0de615d8ef5b
STarrant <stephenvtarrant@gmail.com>
2021-10-14 22:37:26 +0000

    Feat: products/admin.py - Products and product categories fields added to list_display

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,38 @@
+# products/admin.py
+
 from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    A class that extends the ModelAdmin class.
+    Tells the Admin which product fields to display.
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    A class that extends the ModelAdmin class.
+    Tells the Admin which category fields to display.
+    """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
