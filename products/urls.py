79db02944cd3d94462f1c3da4cf6eb688c3d1734
STarrant <stephenvtarrant@gmail.com>
2021-10-16 22:16:41 +0000

    Feat:  Product Detail page added
    - product/urls.py
       - path added for product_detail page
    - product/views.py
       - product_detail function added
    - product/products.html
       - product_detail.html page created from products.html
    - product/products.html
       - urls for product_detail page added

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
-#products/urls.py
+# products/urls.py
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
