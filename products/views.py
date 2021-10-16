427f74c1b9de4b53e30a0eca22af668d4046a72a
STarrant <stephenvtarrant@gmail.com>
2021-10-16 23:44:35 +0000

    Midwork Save:  Bug in main-nav to be resolved

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -11,9 +11,11 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
-    # Return search results if user enters search criterion into search form.
+    # If using seach bar or menu selections
     if request.GET:
+        # Return search results if user enters search criterion into search form.
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,11 +25,17 @@ def all_products(request):
             # Search term in product name or description.
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
+        
+        # Return filtered results if
+        if category in request.GET:
+            category = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
