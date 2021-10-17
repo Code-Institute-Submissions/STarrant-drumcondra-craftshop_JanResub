a9476e6cd9fcae44e3a50b04daa2adc45e033666
STarrant <stephenvtarrant@gmail.com>
2021-10-17 21:21:34 +0000

    Feat:  basket functionality added to basket/contexts.py

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+from decimal import Decimal
 
 def basket_contents(request):
     """ Context processor for shopping basket. """
@@ -6,6 +9,20 @@ def basket_contents(request):
     basket_items_list = []
     total_value = 0
     item_count = 0
+    line_value = 0.00
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        line_value = quantity * product.price
+        total_value += line_value
+        item_count += quantity
+        basket_items_list.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+            'line_value': line_value,
+        })
 
     context = {
         "basket_items_list": basket_items_list,
