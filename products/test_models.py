550cfd049d9284edaf9305ed39f733379a412999
STarrant <stephenvtarrant@gmail.com>
2021-10-17 10:01:42 +0000

    Test:  added test for product/models.py to check string method returns correct name.

---
+++ products/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .models import Category, Product, Creator
+
+# Create your tests here.
+
+
+class TestModels(TestCase):
+
+    def test_category_string_method_returns_name(self):
+        category = Category.objects.create(name='Test Category')
+        self.assertEqual(str(category), 'Test Category')
+
+
+    def test_product_string_method_returns_name(self):
+        product = Product.objects.create(name='Test Product', price=1.00)
+        self.assertEqual(str(product), 'Test Product')
+
+    def test_creator_string_method_returns_name(self):
+        creator = Creator.objects.create(name='Test Creator')
+        self.assertEqual(str(creator), 'Test Creator')
