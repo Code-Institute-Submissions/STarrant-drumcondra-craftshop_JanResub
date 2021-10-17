a174d13dfa37f30e51a604640fbf8833edfb81d6
STarrant <stephenvtarrant@gmail.com>
2021-10-17 08:55:14 +0000

    Feat:  Test for products page added

--- home/tests.py
+++ home/tests.py
@@ -2,7 +2,17 @@ from django.test import TestCase
 
 # Create your tests here.
 
+# url response tests
+
 class HomePageTest(TestCase):
+    """ Test for homepage response. """
     def test_home_page_status_code(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+class ProductPageTest(TestCase):
+    """ Test for products page response. """
+    def test_home_page_status_code(self):
+        response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
