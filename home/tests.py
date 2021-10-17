944a2f1e4c3dd5c8f511d3ab2b1c72c9f567debf
STarrant <stephenvtarrant@gmail.com>
2021-10-17 09:15:09 +0000

    Bug:  Failed test for product creation. Park for now.

--- home/tests.py
+++ home/tests.py
@@ -16,3 +16,4 @@ class ProductPageTest(TestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, 200)
+
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+
+class ProductDetailPageTest(TestCase):
+    """ Test for products page response. """
+    def setUp(self):
+        Product.objects.create(name='test_product', price='1.00')
+    
+    def test_product_creation(self):
+        product=Product.objects.get(id=1)
+        expected_object_name = f'(product.name)'
+        self.assertEqual(expected_object_name, 'test_product')
