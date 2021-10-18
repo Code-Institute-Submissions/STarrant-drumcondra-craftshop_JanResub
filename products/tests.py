11ba4a3565337b45dc49d6129dc8fc44fa7c0e58
STarrant <stephenvtarrant@gmail.com>
2021-10-18 21:54:23 +0000

    Minor:  Comment out of erroneous test products/test.py

--- products/tests.py
+++ products/tests.py
@@ -8,7 +8,7 @@ class ProductDetailPageTest(TestCase):
     def setUp(self):
         Product.objects.create(name='test_product', price='1.00')
     
-    def test_product_creation(self):
-        product=Product.objects.get(id=1)
-        expected_object_name = f'(product.name)'
-        self.assertEqual(expected_object_name, 'test_product')
+#    def test_product_creation(self):
+#        product=Product.objects.get(id=1)
+#        expected_object_name = f'(product.name)'
+#        self.assertEqual(expected_object_name, 'test_product')
