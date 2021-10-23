3527ba5baac2f4998f60201525dbb28a41b8e71a
STarrant <stephenvtarrant@gmail.com>
2021-10-23 08:58:51 +0000

    Feat:  settings.py SECRET_KEY removed

--- drumcondra_craftshop/settings.py
+++ drumcondra_craftshop/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-\
-    h2&@(xj3k$*$b_ve%%7ar^83a8%_ed=osiv+#%td((w1odkd#i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['drumcondra-craftshop.herokuapp.com', 'localhost']
 
