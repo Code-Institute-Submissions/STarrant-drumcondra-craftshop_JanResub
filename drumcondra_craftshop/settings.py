86c688d60bd65752049a18505935bf7e8b701490
STarrant <stephenvtarrant@gmail.com>
2021-10-30 13:16:40 +0000

    Feat:  Debug turned on in settings for CSS testing

--- drumcondra_craftshop/settings.py
+++ drumcondra_craftshop/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')   TestHigh
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '_') # testhigh - remove underscore from secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True # testhigh
 
 ALLOWED_HOSTS = ['drumcondra-craftshop.herokuapp.com', 'localhost']
 
