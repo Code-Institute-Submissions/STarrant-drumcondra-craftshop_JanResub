f7ff89ef11ac18d5305e7eacc3f2550628b3d782
STarrant <stephenvtarrant@gmail.com>
2021-10-30 16:33:16 +0000

    Feat:  Add Creator name to product heading in all products view.

--- products/views.py
+++ products/views.py
@@ -61,6 +61,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_creators': creators,
         'current_sorting': current_sorting,
     }
 
