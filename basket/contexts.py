b0161a1d59e1ebec7a8775ebbac234b60808a699
STarrant <stephenvtarrant@gmail.com>
2021-10-30 21:12:15 +0000

    Feat:  Basket product list fixed to show new database structured items.

--- basket/contexts.py
+++ basket/contexts.py
@@ -8,18 +8,21 @@ def basket_contents(request):
     basket_items_list = []
     total_value = 0
     item_count = 0
+    unit_price = 0.00
     line_value = 0.00
     basket = request.session.get('basket', {})
 
     for item_id, quantity in basket.items():
         product = get_object_or_404(Product, pk=item_id)
-        line_value = quantity * product.price
+        unit_price = product.item_id.unitcost * (1 + product.salesmargin)
+        line_value = quantity * unit_price
         total_value += line_value
         item_count += quantity
         basket_items_list.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
+            'unit_price': unit_price,
             'line_value': line_value,
         })
 
