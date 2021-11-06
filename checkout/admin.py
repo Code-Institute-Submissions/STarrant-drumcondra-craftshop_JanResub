4dbe1a4cb6c6f32491dc1549853bc01d6976e195
STarrant <stephenvtarrant@gmail.com>
2021-11-06 08:52:06 +0000

    Feat:  checkout/admin.py all fields corrected and PEP8 fixed also.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,5 @@
+# checkout/admin.py
+
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
@@ -12,16 +14,19 @@ class OrderLineItemAdminInLine(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInLine,)
 
-    readonly_fields = ('order_no', 'order_date',
-                       'delivery_cost', 'order_total',
-                       'grand_total',)
-    
+    readonly_fields = ('order_no', 'order_date', 'order_weight_g',
+                       'order_items_ship_in_packet', 'delivery_cost',
+                       'order_total', 'grand_total',)
+
     fields = ('order_no', 'order_date', 'full_name', 'email', 
               'phone_no', 'address_street_1', 'address_street_2',
-              'address_town_city', 'address_postcode', 'address_country', 
-              'order_total','delivery_cost', 'grand_total')
-    
-    list_display = ('order_no', 'order_date', 'full_name', 'email', 
-                    'order_total','delivery_cost', 'grand_total')
+              'address_town_city', 'address_postcode', 'address_country',
+              'order_weight_g', 'order_items_ship_in_packet',
+              'order_total', 'delivery_cost', 'grand_total')
+
+    list_display = ('order_no', 'order_date', 'full_name', 'email',
+                    'order_weight_g', 'order_items_ship_in_packet',
+                    'order_total', 'delivery_cost', 'grand_total')
+
 
 admin.site.register(Order, OrderAdmin)
