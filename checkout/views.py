e1681c47ca2bdda8b4b917575fbac811157575a7
STarrant <stephenvtarrant@gmail.com>
2021-11-09 06:55:38 +0000

    Feat:  stripe_public_key added to checkout/views.py

--- checkout/views.py
+++ checkout/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
 
 # Create your views here.
+
 
 def checkout(request):
     basket = request.session.get('basket', {})
@@ -17,6 +19,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51JtLsMCzvnOhwero3k5nYnyBD8U4kGY2tn0yEQaI5EKcCGgmzN4pU63dq3FQntS7PmIMjgfyA9gDH8HJOLJfP8a200xy55ihKq',
+        'client_secret': 'client secret test value'
     }
 
     return render(request, template, context)
