c2872204f03a2d1ab4574c88d72bb80eecd4bef7
STarrant <stephenvtarrant@gmail.com>
2021-11-10 11:24:48 +0000

    Feat: Debug removed from settings.py

--- drumcondra_craftshop/settings.py
+++ drumcondra_craftshop/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '_') # testhigh - remove underscore from secret key
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True # testhigh
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['drumcondra-craftshop.herokuapp.com', 'localhost']
 
