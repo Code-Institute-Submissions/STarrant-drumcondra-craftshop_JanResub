e3c7f9ae9adc11f0e093a3583986f8d7f2de1613
STarrant <stephenvtarrant@gmail.com>
2021-12-30 22:01:56 +0000

    Bug Fix: stripe_elements.js error in placement of style settings for card element.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,7 +5,7 @@ let stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
 let client_secret = $('#id_client_secret').text().slice(1,-1);
 let stripe = Stripe(stripe_public_key);
 let elements = stripe.elements();
-let card = elements.create('card');
+let card = elements.create('card', {style: style});
 
 let style = {
     base: {
@@ -24,4 +24,4 @@ let style = {
         }
     };
 
-card.mount('#card-element', {style: style});
+card.mount('#card-element');
