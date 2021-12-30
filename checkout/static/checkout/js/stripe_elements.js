91653f88a96df421e7da5a76cf2b5233bafc1076
STarrant <stephenvtarrant@gmail.com>
2021-12-30 21:35:59 +0000

    Feat: Stripe Form added to Checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,13 +1,13 @@
 /* Code Credit ChrisZ - Boutique Ado Project as per README */
 
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
-var client_secret = $('#id_client_secret').text().slice(1,-1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var card = elements.create('card');
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1,-1);
+let client_secret = $('#id_client_secret').text().slice(1,-1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+let card = elements.create('card');
 
-var style = {
+let style = {
     base: {
         color: "#000",
         fontFamily: 'Arial, sans-serif',
