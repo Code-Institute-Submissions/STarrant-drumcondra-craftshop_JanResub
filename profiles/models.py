c85888d72fad068ca71ca04eb872bc0776af9abe
STarrant <stephenvtarrant@gmail.com>
2022-01-23 04:38:19 +0000

    Feat: Profiles models, views urls and forms added

--- profiles/models.py
+++ profiles/models.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
     '''
     User profile model for holding personal details,
@@ -14,14 +15,19 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone_no = models.CharField(max_length=20, null=True, blank=True)
-    default_address_street_1 = models.CharField(max_length=80, null=True, blank=True)
-    default_address_street_2 = models.CharField(max_length=80, null=True, blank=True)
-    default_address_town_city = models.CharField(max_length=40, null=True, blank=True)
-    default_address_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_address_street_1 = models.CharField(max_length=80,
+                                                null=True, blank=True)
+    default_address_street_2 = models.CharField(max_length=80,
+                                                null=True, blank=True)
+    default_address_town_city = models.CharField(max_length=40,
+                                                 null=True, blank=True)
+    default_address_postcode = models.CharField(max_length=20,
+                                                null=True, blank=True)
     default_address_country = CountryField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
-# Create your views here.
+from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
@@ -10,9 +13,39 @@ def profile(request):
     '''
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully.')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_no):
+    '''
+    view to create order history.
+    '''
+    order = get_object_or_404(Order, order_no=order_no)
+
+    messages.info(request, (
+        f'This is a history confirmation for order number, {order_no}.\
+            A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
     }
 
     return render(request, template, context)
